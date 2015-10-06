3e2ae9354da0cb0b05a6193b6eb72332c0c031da
Stéphane HULARD <s.hulard@chstudio.fr>
2015-10-06 23:39:12 +0200

    Add a Version definition object with the bumper

---
+++ src/Version.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Stephane HULARD <s.hulard@chstudio.fr>
+ * @package GitServiceMove
+ */
+namespace GitServiceMove;
+
+/**
+ * Version definition
+ * @package GitServiceMove
+ */
+class Version
+{
+    /**
+     * @var string
+     */
+    const REVISION = '0.0.0';
+
+    /**
+     * @var string
+     */
+    const NAME = 'Git service move';
+
+    /**
+     * Version wrapper constructor
+     */
+    protected function __construct()
+    {}
+}
