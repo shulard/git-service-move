8e781167d77ae4012447919b4adefc1d1ef01bc9
Stéphane HULARD <s.hulard@chstudio.fr>
2015-10-07 00:13:19 +0200

    Add an abstract command layer

---
+++ src/Command/AbstractCommand.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Stephane HULARD <s.hulard@chstudio.fr>
+ * @package GitServiceMove\Command
+ */
+namespace GitServiceMove\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Global command canvas
+ * @package GitServiceMove\Command
+ */
+class AbstractCommand extends Command
+{
+    const CODE_ABORTED = 100;
+    const CODE_ERROR = 1;
+    const CODE_OK = 0;
+
+    /**
+     * Basic configuration for beebot command.
+     * Define standard and globals options for the command line
+     */
+    protected function configure()
+    {
+        //Dynamically define command name
+        $baseName = explode('\\', substr(get_called_class(), strlen(__NAMESPACE__)+1));
+        array_walk($baseName, function (&$v) {
+            $v = strtolower($v);
+        });
+        $this->setName(implode(':', $baseName));
+    }
+}
